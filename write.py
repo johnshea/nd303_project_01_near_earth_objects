2f8e2e9f58c995381ee9520d663043db5c222b85
John Shea <sheajohnh@gmail.com>
2022-01-03 17:04:29 -0500

    Implement write_to_csv and write_to_file

--- write.py
+++ write.py
@@ -12,7 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
-
+import helpers
 
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
@@ -28,7 +28,30 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+
+    with open(filename, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(fieldnames)
+
+        for row in results:
+            result_dict = {field: None for field in fieldnames}
+
+            result_dict['datetime_utc'] = helpers.datetime_to_str(row.time)
+            result_dict['distance_au'] = row.distance
+            result_dict['velocity_km_s'] = row.velocity
+            result_dict['designation'] = row.neo.designation
+ 
+            if result_dict['name'] is None:
+                result_dict['name'] = ''
+
+            if row.neo.diameter == float('nan'):
+                result_dict['diameter_km'] = 'nan'
+            else:
+                result_dict['diameter_km'] = row.neo.diameter
+
+            result_dict['potentially_hazardous'] = row.neo.hazardous
+
+            writer.writerow(result_dict.values())
 
 
 def write_to_json(results, filename):
@@ -42,4 +65,37 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'neo')
+
+    data = []
+
+    for row in results:
+        result_dict = {field: None for field in fieldnames}
+
+        result_dict['datetime_utc'] = helpers.datetime_to_str(row.time)
+        result_dict['distance_au'] = row.distance
+        result_dict['velocity_km_s'] = row.velocity
+
+        neo_dict = {'designation': None, 'name': None, 'diameter_km': None, 'potentially_hazardous': None}
+
+        neo_dict['designation'] = row.neo.designation
+
+        if row.neo.name is None:
+            neo_dict['name'] = ''
+        else:
+            neo_dict['name'] = row.neo.name
+
+        if row.neo.diameter == float('nan'):
+            neo_dict['diameter_km'] = float('nan')
+        else:
+            neo_dict['diameter_km'] = row.neo.diameter
+
+        neo_dict['potentially_hazardous'] = row.neo.hazardous
+
+        result_dict['neo'] = neo_dict
+
+        data.append(result_dict)
+
+    # print("data: ", data)
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
