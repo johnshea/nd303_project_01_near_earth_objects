8176eb73d449d8a42d282165eb97e202c585c034
John Shea <sheajohnh@gmail.com>
2022-01-04 13:54:27 -0500

    Fix code and docstring style issues in write.py

--- write.py
+++ write.py
@@ -14,15 +14,17 @@ import csv
 import json
 import helpers
 
+
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
 
-    The precise output specification is in `README.md`. Roughly, each output row
-    corresponds to the information in a single close approach from the `results`
-    stream and its associated near-Earth object.
+    The precise output specification is in `README.md`. Roughly, each output
+    row corresponds to the information in a single close approach from the
+    `results` stream and its associated near-Earth object.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the data should be
+        saved.
     """
     fieldnames = (
         'datetime_utc', 'distance_au', 'velocity_km_s',
@@ -40,7 +42,7 @@ def write_to_csv(results, filename):
             result_dict['distance_au'] = row.distance
             result_dict['velocity_km_s'] = row.velocity
             result_dict['designation'] = row.neo.designation
- 
+
             if result_dict['name'] is None:
                 result_dict['name'] = ''
 
@@ -57,13 +59,14 @@ def write_to_csv(results, filename):
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
 
-    The precise output specification is in `README.md`. Roughly, the output is a
-    list containing dictionaries, each mapping `CloseApproach` attributes to
+    The precise output specification is in `README.md`. Roughly, the output is
+    a list containing dictionaries, each mapping `CloseApproach` attributes to
     their values and the 'neo' key mapping to a dictionary of the associated
     NEO's attributes.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the data should be
+        saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'neo')
 
@@ -76,7 +79,10 @@ def write_to_json(results, filename):
         result_dict['distance_au'] = row.distance
         result_dict['velocity_km_s'] = row.velocity
 
-        neo_dict = {'designation': None, 'name': None, 'diameter_km': None, 'potentially_hazardous': None}
+        neo_dict = {
+            'designation': None, 'name': None,
+            'diameter_km': None, 'potentially_hazardous': None
+        }
 
         neo_dict['designation'] = row.neo.designation
 
